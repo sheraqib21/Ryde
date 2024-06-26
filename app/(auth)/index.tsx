a45ea4bb8d22d77328aaa3b348892050b075d2ee
ihaseebullah <ihaseebullahtarakai@gmail.com>
2024-06-26 08:58:42 +0500

    Signout functionality enabled

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { useAuth } from '@clerk/clerk-expo'
 
 const index = () => {
+  const {signOut}=useAuth()
   return (
     <View>
       <Text>index</Text>
+      <Button onPress={()=>signOut()} title='Signout'/>
     </View>
   )
 }
